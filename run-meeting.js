f260797ee2a6f3a7745d06f2b7e933ac2a81ac20
minoxd <minophen.aceta114@gmail.com>
2024-10-14 01:26:49 +0700

    update

--- run-meeting.js
+++ run-meeting.js
@@ -1,21 +1,31 @@
-import {By, until} from 'selenium-webdriver';
-import {rm, readFile, readdir} from 'fs/promises';
+import {Builder, By, until} from 'selenium-webdriver';
+import {rm, readFile, readdir, cp} from 'fs/promises';
 import axios from "axios";
 import {sp} from './constants.js'
+import {delay} from "./utils.js";
+import chrome from "selenium-webdriver/chrome.js";
 
 //selenium + chrome, host chat-api-dev.campdi.vn
 //FRecode extension for chrome
 //trick lo clone user data to save login session
-export default async function (driver, eventId, url, duration) {
-    const endTimeSimulate = 10000
-
+export async function runSeparateBrowserInstances(eventId, url, duration) {
+    const endTimeSimulate = 10000;
+    // Specify the path to ChromeDriver and Chrome user data directory
+    let userDataPath = 'C:/Users/leduc/AppData/Local/Google/Chrome/User Data';
+    let cloneUserDataPath = 'C:/Users/leduc/Desktop/v/' + new Date().getTime() + '_' + eventId;
     let scriptPath = sp;
     let meetingName = 'MeetingNote-' + url.substring(24)
     console.log(meetingName)
+    await cp(userDataPath, cloneUserDataPath, {recursive: true});
+    let options = new chrome.Options();
+    options.addArguments(`--user-data-dir=` + cloneUserDataPath);
+
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
 
     try {
-        // new tab
-        await driver.switchTo().newWindow('tab');
         // Perform actions with the WebDriver, e.g., navigate to a page
         await driver.get(url);
 
@@ -71,5 +81,65 @@ export default async function (driver, eventId, url, duration) {
 
         }, duration + 60000); // 1min on top of call
     }
-};
+}
+
+export async function runMultipleTabs(driver, url, duration) {
+    await driver.switchTo().newWindow('tab');
+    const thisWindowHandler = await driver.getWindowHandle();
+    try {
+        await driver.get(url);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//button[span='Join now']"))), 600000);
+        await driver.findElement(By.xpath("//button[span='Join now']")).click();
+        // confirm meet connection
+        await delay(4000)
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // Quit the driver
+        setTimeout(async function () {
+            await driver.switchTo().window(thisWindowHandler);
+            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//button[@aria-label='Leave call']"))), 10000);
+            await driver.findElement(By.xpath("//button[@aria-label='Leave call']")).click();
+
+            // setTimeout(async function () {
+            //     await driver.quit();
+            //
+            //     // setTimeout(async function () {
+            //     //     await rm(cloneUserDataPath, {recursive: true});
+            //     //
+            //     //     //fs get all files in directory
+            //     //     const files = await readdir(scriptPath, {withFileTypes: true});
+            //     //     const txtFiles =
+            //     //         files.filter(file => file.isFile() && file.name.startsWith(meetingName) && file.name.endsWith('.txt'))
+            //     //             .sort((a, b) => b.mtime - a.mtime)
+            //     //             .at(0);
+            //     //
+            //     //     console.log(txtFiles.name)
+            //     //     //read txt file
+            //     //     const content = await readFile(scriptPath + txtFiles.name, 'utf8');
+            //     //     //console.log(content)
+            //     //     //submit data
+            //     //     const options = {
+            //     //         method: 'POST',
+            //     //         url: 'http://chat-api-dev.campdi.vn/api/meeting/summary',
+            //     //         data: {
+            //     //             "eventId": eventId,
+            //     //             "summary": content
+            //     //         }
+            //     //     };
+            //     //
+            //     //     try {
+            //     //         const {data} = await axios.request(options);
+            //     //         console.log(data);
+            //     //     } catch (error) {
+            //     //         console.error("failed" + error);
+            //     //     }
+            //     //
+            //     // }, 3000)
+            //
+            // }, 60000) //1min turn off call to quit driver wait for summary file
+
+        }, duration + 60000); // 1min on top of call
+    }
+}
 
--- schedule.js
+++ schedule.js
@@ -1,48 +1,63 @@
 import {ToadScheduler, SimpleIntervalJob, AsyncTask} from 'toad-scheduler';
-import select from './query/select.js';
-import runMeeting from './run-meeting.js';
-import updateMeetingStarted from "./query/updateMeetingStarted.js";
-import {cudp, udp} from "./constants.js";
+import select from "./query/select.js";
+import {cudp, MeetChoice, udp} from "./constants.js";
 import {cp} from "fs/promises";
 import chrome from "selenium-webdriver/chrome.js";
 import {Builder} from "selenium-webdriver";
+import updateMeetingStarted from "./query/updateMeetingStarted.js";
+import {runMultipleTabs, runSeparateBrowserInstances} from "./run-meeting.js";
 
-const scheduler = new ToadScheduler()
+const scheduler = new ToadScheduler();
 
 const task = new AsyncTask(
     'simple task',
     async () => {
         const results = await select();
+        const meetChoice = 1; // 1 or 2
+        switch (meetChoice) {
+            case MeetChoice.SeparateBrowserInstances:
+                for (let i = 0; i < results.length; i++) {
+                    const meeting = results[i];
+                    console.log("meeting about to start id=" + meeting.id + " url=" + meeting.meet_url);
+                    if (new Date(meeting.start_time) >= new Date()) {
+                        continue;
+                    }
+                    const duration = new Date(meeting.end_time) - new Date(meeting.start_time);
+                    await runSeparateBrowserInstances(meeting.id_event, meeting.meet_url, duration)
 
-        // Specify the path to ChromeDriver and Chrome user data directory
-        let userDataPath = udp;
-        let cloneUserDataPath = cudp + new Date().getTime();
-        await cp(userDataPath, cloneUserDataPath, {recursive: true});
+                    // set status to meeting started
+                    updateMeetingStarted(meeting.id);
+                }
+                break;
+            case MeetChoice.MultipleTabs:
+                let userDataPath = udp;
+                let cloneUserDataPath = cudp + new Date().getTime();
+                await cp(userDataPath, cloneUserDataPath, {recursive: true});
 
-        let options = new chrome.Options();
-        options.addArguments(`--user-data-dir=` + cloneUserDataPath);
+                let options = new chrome.Options();
+                options.addArguments(`--user-data-dir=` + cloneUserDataPath);
 
-        let driver = await new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(options)
-            .build();
-
-        for (let i = 0; i < results.length; i++) {
-            const meeting = results[i];
-            console.log("meeting about to start id=" + meeting.id + " url=" + meeting.meet_url);
-            if (new Date(meeting.start_time) < new Date()) {
-                const duration = new Date(meeting.end_time) - new Date(meeting.start_time);
-                await runMeeting(driver, meeting.id_event, meeting.meet_url, duration)
-
-                // set status to meeting started
-                updateMeetingStarted(meeting.id);
-            }
+                let driver = await new Builder()
+                    .forBrowser('chrome')
+                    .setChromeOptions(options)
+                    .build();
+                for (let i = 0; i < results.length; i++) {
+                    const meeting = results[i];
+                    console.log("meeting about to start id=" + meeting.id + " url=" + meeting.meet_url);
+                    if (new Date(meeting.start_time) >= new Date()) {
+                        continue;
+                    }
+                    const duration = new Date(meeting.end_time) - new Date(meeting.start_time);
+                    await runMultipleTabs(driver, meeting.meet_url, duration);
+                }
+                break;
         }
+
     },
     (err) => {
         console.log("Error at time " + err)
     }
 )
-const job = new SimpleIntervalJob({seconds: 10}, task)
+const job = new SimpleIntervalJob({seconds: 10}, task);
 
-scheduler.addSimpleIntervalJob(job)
+scheduler.addSimpleIntervalJob(job);
