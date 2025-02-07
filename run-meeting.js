912ed82fc71eb70393f6e2bfbc7ed68f12a66267
khangld5 <khangld5@fpt.com>
2025-02-07 16:08:44 +0700

    refactor code

--- run-meeting.js
+++ run-meeting.js
@@ -8,7 +8,7 @@ import parsedDate from "./parse-date.js";
 //FRecode extension for chrome
 //trick lo clone user data to save login session
 export default async function (eventId, url, duration) {
-    const endTimeSimulate = 10000
+    const extendedTime = 60000;
     // Specify the path to ChromeDriver and Chrome user data directory
     let userDataPath = 'C:/Users/leduc/AppData/Local/Google/Chrome/User Data';
     let cloneUserDataPath = 'C:/Users/leduc/Desktop/v/' + new Date().getTime() + '_' + eventId;
@@ -30,12 +30,14 @@ export default async function (eventId, url, duration) {
         // Perform actions with the WebDriver, e.g., navigate to a page
         await driver.get(url);
 
-        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//button[span='Join now']"))), 600000)
+        await driver.manage().setTimeouts({implicit: 2000}); //not sure
+
+        await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//button[span='Join now']"))), 60000)
 
         await driver.findElement(By.xpath("//button[span='Join now']")).click();
 
         let loop = true
-        // wait to admit newcomers
+        // wait to admit newcomers and block the final clause for the duration of the meeting
         setTimeout(function () {
             loop = false
         }, duration)
@@ -44,66 +46,61 @@ export default async function (eventId, url, duration) {
             try {
                 console.log("wait to admit newcomers")
                 await driver.findElement(By.xpath("//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-dgl2Hf ksBjEc lKxP2d LQeN7' and @jscontroller='soHxf']")).click()
-                console.log("oh yeah")
             } catch (e) {
                 console.log("no newcomers")
             }
         }
-
-        console.log("its over")
     } finally {
         console.log("finally")
         // Quit the driver
-        setTimeout(async function () {
-            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//button[@aria-label='Leave call']"))), 10000)
 
-            await driver.findElement(By.xpath("//button[@aria-label='Leave call']")).click()
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//button[@aria-label='Leave call']"))), 10000)
+
+        await driver.findElement(By.xpath("//button[@aria-label='Leave call']")).click()
+
+        setTimeout(async function () {
+            await driver.quit();
 
             setTimeout(async function () {
-                await driver.quit();
+                await rm(cloneUserDataPath, {recursive: true});
 
-                setTimeout(async function () {
-                    await rm(cloneUserDataPath, {recursive: true});
+                //fs get all files in directory
+                const files = await readdir(scriptPath, {withFileTypes: true});
+                const txtFiles =
+                    files.filter(file => file.isFile() && file.name.includes(meetingName, 0))
+                        .sort((a, b) => {
+                            const timeA = a.name.substring(meetingName.length, a.name.length - 4) // remove .txt
+                            const timeB = b.name.substring(meetingName.length, b.name.length - 4)
+                            const dateA = parsedDate(timeA)
+                            const dateB = parsedDate(timeB)
+                            return dateB - dateA;
+                        })
+                        .at(0);
 
-                    //fs get all files in directory
-                    const files = await readdir(scriptPath, {withFileTypes: true});
-                    const txtFiles =
-                        files.filter(file => file.isFile() && file.name.includes(meetingName, 0))
-                            .sort((a, b) => {
-                                const timeA = a.name.substring(meetingName.length, a.name.length - 4) // remove .txt
-                                const timeB = b.name.substring(meetingName.length, b.name.length - 4)
-                                const dateA = parsedDate(timeA)
-                                const dateB = parsedDate(timeB)
-                                return dateB - dateA;
-                            })
-                            .at(0);
-
-                    console.log(txtFiles.name)
-                    //read txt file
-                    const content = await readFile(scriptPath + txtFiles.name, 'utf8');
-                    console.log(content)
-                    //submit data
-                    const options = {
-                        method: 'POST',
-                        url: 'http://chat-api-dev.campdi.vn/api/meeting/summary',
-                        data: {
-                            "eventId": eventId,
-                            "summary": content
-                        }
-                    };
-
-                    try {
-                        const {data} = await axios.request(options);
-                        console.log(data);
-                    } catch (error) {
-                        console.error("failed" + error);
+                console.log(txtFiles.name)
+                //read txt file
+                const content = await readFile(scriptPath + txtFiles.name, 'utf8');
+                console.log(content)
+                //submit data
+                const options = {
+                    method: 'POST',
+                    url: 'http://chat-api-dev.campdi.vn/api/meeting/summary',
+                    data: {
+                        "eventId": eventId,
+                        "summary": content
                     }
+                };
 
-                }, 3000)
+                try {
+                    const {data} = await axios.request(options);
+                    console.log(data);
+                } catch (error) {
+                    console.error("failed" + error);
+                }
 
-            }, 60000) //1min turn off call to quit driver
+            }, 3000)
 
-        }, duration + 60000); // 1min on top of call
+        }, 30000) //30s turn off call to quit driver
     }
 };
 
